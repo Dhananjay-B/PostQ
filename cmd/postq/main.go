cb238bcf045f172e6a96291e9da3638aeab0c9b2
Dhananjay Bhujbal <dhananjay.bhujbal19@vit.edu>
2026-02-18 22:45:32 +0530

    Add test fot SSH probe

--- cmd/postq/main.go
+++ cmd/postq/main.go
@@ -1,22 +1,26 @@
 package main
 
 import (
-	"fmt"
-
 	"github.com/Dhananjay-B/PostQ/internal/model"
 	"github.com/Dhananjay-B/PostQ/internal/probe"
 )
 
 func main() {
-	endpoint := model.Endpoint{
-		HostName: "google.com",
-		Port:     443,
-	}
+	// endpoint := model.Endpoint{
+	// 	HostName: "google.com",
+	// 	Port:     443,
+	// }
 
-	result, err := probe.ScanTLS(endpoint)
-	if err != nil {
-		fmt.Println("Error scanning TLS:", err)
-		return
+	// result, err := probe.ScanTLS(endpoint)
+	// if err != nil {
+	// 	fmt.Println("Error scanning TLS:", err)
+	// 	return
+	// }
+	// fmt.Println("TLS Scan Result:", result)
+
+	sshTarget := model.SSHTarget{
+		HostName: "192.168.1.2",
+		Port:     22,
 	}
-	fmt.Println("TLS Scan Result:", result)
+	probe.ScanSSH(sshTarget)
 }
