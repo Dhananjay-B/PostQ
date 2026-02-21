5aa989f18430ddd6fdda281b9986608aceb660b8
Dhananjay Bhujbal <dhananjay.bhujbal19@vit.edu>
2026-02-21 13:34:16 +0530

    add version command. Prepare for v0.1.0

---
+++ cmd/version.go
@@ -0,0 +1,28 @@
+/*
+Copyright © 2026 DHANANJAY BHUJBAL <dhananjay.bhujbal19@vit.edu>
+*/
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+)
+
+var version = "0.1.0"
+
+var versionCmd = &cobra.Command{
+	Use:   "version",
+	Short: "Display version information",
+	RunE: func(cmd *cobra.Command, args []string) error {
+		if version == "" {
+			return fmt.Errorf("version not found")
+		}
+		_, err := fmt.Println(version)
+		return err
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(versionCmd)
+}
