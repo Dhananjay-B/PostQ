49179e9844e72e30be4e85a654f65e099a7a3968
Dhananjay Bhujbal <dhananjay.bhujbal19@vit.edu>
2026-05-17 10:54:25 +0530

    Add cobra-cli for smtp scan

---
+++ cmd/smtp.go
@@ -0,0 +1,66 @@
+/*
+Copyright © 2026 DHANANJAY BHUJBAL <dhananjay.bhujbal19@vit.edu>
+*/
+package cmd
+
+import (
+	"encoding/json"
+	"net"
+	"os"
+	"strconv"
+
+	smtpmodels "github.com/Dhananjay-B/PostQ/internal/model/smtpmodels"
+	probes "github.com/Dhananjay-B/PostQ/internal/probe"
+	"github.com/spf13/cobra"
+)
+
+// smtpCmd represents the smtp command
+var smtpCmd = &cobra.Command{
+	Use:   "smtp [host:port]",
+	Short: "Scan SMTP endpoint with STARTTLS support",
+	Long: `Scan SMTP host to get cryptographic inventory.
+SMTP scan makes raw TCP connection to remote host on specified port and collects below information
+- SMTP Banner
+- SMTP Software
+- ESMTP Support
+- STARTTLS Support
+- Supported TLS Versions
+- Supported Cipher Suites
+- Server Cipher Preference
+- Certificate Chain
+- OCSP Status`,
+	Args: cobra.ExactArgs(1),
+	RunE: func(cmd *cobra.Command, args []string) error {
+
+		target := args[0]
+
+		host, portStr, err := net.SplitHostPort(target)
+		if err != nil {
+			return err
+		}
+
+		port, err := strconv.Atoi(portStr)
+		if err != nil {
+			return err
+		}
+
+		smtpTarget := smtpmodels.SMTPTarget{
+			HostName: host,
+			Port:     port,
+		}
+
+		result, err := probes.ScanSMTP(smtpTarget)
+		if err != nil {
+			return err
+		}
+
+		enc := json.NewEncoder(os.Stdout)
+		enc.SetIndent("", "  ")
+
+		return enc.Encode(result)
+	},
+}
+
+func init() {
+	scanCmd.AddCommand(smtpCmd)
+}
